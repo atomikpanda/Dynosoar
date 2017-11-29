91cb7fc4a77a50af4b5d47c3c47846a2787dda11
Bailey Seymour (atomikpanda) <b.seymour@me.com>
2017-11-29 16:21:54 -0500

    Initial Commit

---
+++ Dynosoar/Dynosoar.h
@@ -0,0 +1,19 @@
+//
+//  Dynosoar.h
+//  Dynosoar
+//
+//  Created by Bailey Seymour on 11/29/17.
+//  Copyright © 2017 Bailey Seymour. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Dynosoar.
+FOUNDATION_EXPORT double DynosoarVersionNumber;
+
+//! Project version string for Dynosoar.
+FOUNDATION_EXPORT const unsigned char DynosoarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Dynosoar/PublicHeader.h>
+
+
