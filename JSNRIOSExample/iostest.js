c570dbf57941f379d1cb06161e3811dc9effceb6
Bailey Seymour (atomikpanda) <b.seymour@me.com>
2017-12-06 10:50:48 -0500

    UILabel example

--- JSNRIOSExample/iostest.js
+++ JSNRIOSExample/iostest.js
@@ -2,27 +2,13 @@
 interface("ViewController")
 interface("JSNRContext")
 interface("UIColor")
-
+interface("UILabel")
 
 hook(ViewController,"viewDidLoad", function(self,cmd){
-
+     var label = UILabel()
+     label.text = "this is a sample label";
+     label.frame = [0,0,300,300];
+     self.view().addSubview$(label);
      self.view().backgroundColor = UIColor.redColor();
 });
-// hook(AppDelegate,"applicationDidFinishLaunching:", function(self,cmd,notification){
-//
-//      self.$.makeRed();
-//      var bundle = C.NSBundle.mainBundle();
-//      console.log("bundle: "+bundle.toString());
-//      var bid = bundle.bundleIdentifier()
-//      console.log("WE DID IT! "+bid);
-//      self = self.$
-//      self.makePurple$$(bid)
-//      // self.window.setBackgroundColor$$(cls("NSColor").$.orangeColor())
-//      self.window().title = "This calls to objc"
-//      // self.title = "lololool"
-//      // var window = self.window()
-//      self.window().center()
-// //     console.log(__self.$.allMaps)
-//     orig(self,"orig_"+cmd,notification);
-//
-// });
+
